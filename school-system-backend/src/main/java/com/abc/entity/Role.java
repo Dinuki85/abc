92e9fecda959efe160c3ce2c9c9d1cb9de6641c1
Dinuki85 <prarthanadinuki2@gmail.com>
2026-03-31 17:11:39 +0530

    add enums for role and verification status

---
+++ school-system-backend/src/main/java/com/abc/entity/Role.java
@@ -0,0 +1,5 @@
+package com.abc.entity;
+
+public enum Role {
+    STUDENT, STAFF, TEACHER
+}
---
+++ school-system-backend/src/main/java/com/abc/entity/VerificationStatus.java
@@ -0,0 +1,5 @@
+package com.abc.entity;
+
+public enum VerificationStatus {
+    PENDING, VERIFIED, NEEDS_CORRECTION
+}
