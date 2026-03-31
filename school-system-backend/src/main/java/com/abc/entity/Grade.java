d22dd3eba98cc15e74d8f9830893c68f98474460
Dinuki85 <prarthanadinuki2@gmail.com>
2026-03-31 17:11:23 +0530

    create grade entity

---
+++ school-system-backend/src/main/java/com/abc/entity/Grade.java
@@ -0,0 +1,18 @@
+package com.abc.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "grades")
+public class Grade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
