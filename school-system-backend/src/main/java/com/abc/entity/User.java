c5303aa7ae4929fbe4e8c6d72383fc329ef2717b
Dinuki85 <prarthanadinuki2@gmail.com>
2026-03-31 17:11:13 +0530

    create user entity

---
+++ school-system-backend/src/main/java/com/abc/entity/User.java
@@ -0,0 +1,28 @@
+package com.abc.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "is_first_login")
+    private boolean isFirstLogin = true;
+}
