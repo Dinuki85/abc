f2b758d2f44e3afbfcafaa3761f988fae3d59b05
Dinuki85 <prarthanadinuki2@gmail.com>
2026-04-21 20:40:41 +0530

    Delete:Deleted Unnecessary files

--- school-system-backend-new/src/main/java/com/school/sms/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.school.sms.model;
-
-public enum Role {
-    ADMIN,
-    STUDENT,
-    STAFF
-}
--- school-system-backend-new/src/main/java/com/school/sms/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.school.sms.repository;
-
-import com.school.sms.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
